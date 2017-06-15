2e6397523960e1f6edf11dc90bd60e4e986f2d35
Alex White <Alex.White@unison.co.nz>
2017-06-16 11:12:40 +1200

    Ellipsity version 1.0

---
+++ README.md
@@ -0,0 +1,31 @@
+# Ellipsity
+A JavaScript library that Ellipsis multiline text that overflows its container. Simple, Fast, Powerful!
+
+*Will dynamically ellipsis multiline text regardless of the amount of lines the text contains to the size of its parent container.
+
+## Code Example
+```javascript
+Ellipsity.ellipsify(document.getElementById("container"));
+```
+
+Is all that is required, view the example for a live example.
+
+![Alt text](example.png?raw=true "Ellips Example")
+
+## Motivation
+HTML, CSS and Javascript do not support native crossbrowser functionality to truncate and ellips text. This library is a simple and easy way to provide that functionality for you and the code can easily be built on.
+
+## Installation
+Include the following script into the head or body of your html document:
+```html
+<script type="text/javascript" src="ellipsity.min.js"></script>
+```
+
+## API Reference
+You are set with: 
+```javascript
+Ellipsity.ellipsify(container);
+```
+
+## License
+The current license is MIT as it is the simplest and less restrictive license I could find on here at the time. If you feel like it is restricting you in anyway I am open to alternate licenses that are less restricting.
---
+++ ellipsity.js
@@ -0,0 +1,57 @@
+//Ellipsify text that overflows its container.
+var Ellipsity = (function(){
+	function Ellipsity(){
+	}
+	
+	//Binary search to find optimal splitting points.
+	Ellipsity.prototype.binarySearch = function(length, callback) {
+		var low = 0;
+		var high = length - 1;
+		var best = -1;
+		var mid;
+
+		while (low <= high) {
+			mid = ~~((low + high) / 2);
+			var result = callback(mid);
+			if (result < 0) {
+				high = mid - 1;
+			} 
+			else if (result > 0) {
+				low = mid + 1;
+			} 
+			else {
+				best = mid;
+				low = mid + 1;
+			}
+		}
+		return best;
+	}
+	
+	//Check to see if the text is overflowing.
+	Ellipsity.prototype.isTextOverflowing = function(element)
+	{
+		var currentOverflow = element.style.overflow;
+		if (!currentOverflow || currentOverflow === "visible")
+		  element.style.overflow = "hidden";
+
+		var isOverflowing = element.clientWidth < element.scrollWidth || element.clientHeight < element.scrollHeight;
+		element.style.overflow = currentOverflow;
+		return isOverflowing;
+	}
+	
+	//Ellipsify the container so the last text element inside fits.
+	Ellipsity.prototype.ellipsify = function(container){
+		var self = this;
+		var content = container.lastElementChild;
+		var text = content.innerText;
+		if(this.isTextOverflowing(container)){
+			var checkFunc = function (i) {
+				content.innerText = text.substring(0, i);
+				return self.isTextOverflowing(container)?-1:0;
+			};
+			var len = this.binarySearch(text.length - 1, checkFunc);
+			content.innerText = text.substring(0, len).slice(0,-3)+"...";
+		}
+	}
+	return new Ellipsity();
+}());
